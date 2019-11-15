78a76d2e8593032e5dd5364b2784903f83a3814f
Vinograd Oleg <rickes.oleg@gmail.com>
2019-11-15 20:32:14 +0300

    fixed construct port issue

--- src/main/java/com/grape/server/Connection.java
+++ src/main/java/com/grape/server/Connection.java
@@ -2,15 +2,18 @@ package com.grape.server;
 
 import lombok.Getter;
 import lombok.SneakyThrows;
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.net.ServerSocket;
 
 @Component
 @Getter
 @PropertySource("classpath:properties/app.properties")
+@Log
 public class Connection {
     private ServerSocket sSocket;
 
@@ -18,7 +21,9 @@ public class Connection {
     private int port;
 
     @SneakyThrows
-    public Connection() {
+    @PostConstruct
+    private void initialize() {
+        log.info("Listening port: " + port);
         sSocket = new ServerSocket(port);
     }
 }
